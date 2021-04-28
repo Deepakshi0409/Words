43ca3208c865ce37e7c10c1461fe2bee14a4e4af
Deepakshi0409 <Deepakshi.deeps@gmail.com>
2021-04-28 12:56:17 +0530

    Queries

---
+++ app/src/main/java/com/example/words/WordDao.kt
@@ -0,0 +1,17 @@
+package com.example.words
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface WordDao {
+    @Query("SELECT * FROM word_table ORDER BY word Asc")
+    fun getAlphabetizedWords(): LiveData<List<Word>>
+
+    @Insert(onConflict =  OnConflictStrategy.IGNORE)
+    suspend fun insert(word :Word)
+
+    @Query("DELETE FROM word_table")
+    suspend fun deleteAll()
+}
