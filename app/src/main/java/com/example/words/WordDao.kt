d8f029f8d84b2abf9aad389a5a9d6f13e9ad9e2e
Deepakshi0409 <Deepakshi.deeps@gmail.com>
2021-04-29 11:42:41 +0530

    Sql queries

--- app/src/main/java/com/example/words/WordDao.kt
+++ app/src/main/java/com/example/words/WordDao.kt
@@ -7,7 +7,7 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface WordDao {
     @Query("SELECT * FROM word_table ORDER BY word Asc")
-    fun getAlphabetizedWords(): LiveData<List<Word>>
+    fun getAlphabetizedWords(): Flow<List<Word>>
 
     @Insert(onConflict =  OnConflictStrategy.IGNORE)
     suspend fun insert(word :Word)
